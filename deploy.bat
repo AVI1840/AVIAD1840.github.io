fbbef7299d8b961585beda27d323d1f03315e773
Aviad Yitzhaki <AVIAD1840@users.noreply.github.com>
2026-03-10 15:35:49 +0200

    Update partners: add Ministry of Health, remove Ministry of Labor and Mosaic

---
+++ deploy.bat
@@ -0,0 +1,24 @@
+@echo off
+echo === Deploy to GitHub Pages ===
+echo.
+
+REM Stage all relevant files
+git add index.html
+git add images/ 2>nul
+git add .gitignore package.json package-lock.json 2>nul
+
+REM Show what will be committed
+git status
+
+echo.
+set /p MSG="Enter commit message (or press Enter for 'Update site'): "
+if "%MSG%"=="" set MSG=Update site
+
+git commit -m "%MSG%"
+
+REM Push
+git push origin main
+
+echo.
+echo Done! Site will update at https://aviad1840.github.io in ~1 minute.
+pause
